0b32861ae76b9840a784b3afc83f63cf20e5b2ad
rishiiiidha <rishidha04@gmail.com>
2024-09-15 17:02:51 +0530

    icons

--- client/src/components/Sidebar/LanguageSelector.jsx
+++ client/src/components/Sidebar/LanguageSelector.jsx
@@ -12,42 +12,42 @@ const LanguageSelector = () => {
   };
 
   return (
-    <div className='relative text-white justify-center items-center'>
-      <div
-        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-        className='flex gap-5 justify-between items-center px-5 py-3 mt-4 font-semibold whitespace-nowrap rounded-3xl border-2 border-solid bg-neutral-700 border-neutral-800 max-md:pl-5 max-md:mr-1 cursor-pointer'
-      >
-        <div className='flex gap-3'>
-          <img
-            loading='lazy'
-            src='https://cdn.builder.io/api/v1/image/assets/TEMP/f22ec49075343e627a4670076c04cdc719b472d9c04e0605f2c4fa565e1b068c?placeholderIfAbsent=true&apiKey=61fdf683f141495eb249129d739ec110'
-            alt=''
-            className='object-contain shrink-0 self-start w-5 aspect-square mt-1'
-          />
-          <span>{selectedLanguage}</span>
-        </div>
-        <img
-          loading='lazy'
-          src='https://cdn.builder.io/api/v1/image/assets/TEMP/d39921f8dab7b0b323c75df18171371b99e54960142f650d14c9353fab5808cd?placeholderIfAbsent=true&apiKey=61fdf683f141495eb249129d739ec110'
-          alt=''
-          className='object-contain shrink-0 aspect-square w-[30px] mt-0.5'
-        />
-      </div>
-      {isDropdownOpen && (
-        <ul className='absolute bottom-full left-0 w-full bg-neutral-800 border border-neutral-700 rounded-md shadow-lg'>
-          {languages.map((language, index) => (
-            <li
-              key={index}
-              onClick={() => handleLanguageSelect(language)}
-              className='px-4 py-2 hover:bg-neutral-700 text-white cursor-pointer'
-            >
-              {language}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+		<div className='relative text-white justify-center items-center'>
+			<div
+				onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+				className='flex gap-5 justify-between items-center px-5 py-3 mt-4 font-semibold whitespace-nowrap rounded-3xl border-2 border-solid bg-neutral-700 border-neutral-800 max-md:pl-5 max-md:mr-1 cursor-pointer'
+			>
+				<div className='flex gap-3'>
+					<img
+						loading='lazy'
+						src='/language.svg'
+						alt=''
+						className='object-contain shrink-0 self-start w-5 aspect-square mt-1'
+					/>
+					<span>{selectedLanguage}</span>
+				</div>
+				<img
+					loading='lazy'
+					src='/language-2.svg'
+					alt=''
+					className='object-contain shrink-0 aspect-square w-[30px] mt-0.5'
+				/>
+			</div>
+			{isDropdownOpen && (
+				<ul className='absolute bottom-full left-0 w-full bg-neutral-800 border border-neutral-700 rounded-md shadow-lg'>
+					{languages.map((language, index) => (
+						<li
+							key={index}
+							onClick={() => handleLanguageSelect(language)}
+							className='px-4 py-2 hover:bg-neutral-700 text-white cursor-pointer'
+						>
+							{language}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
 
 export default LanguageSelector;
